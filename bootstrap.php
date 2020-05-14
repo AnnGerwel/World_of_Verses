a58434dd3400214d5a3d3904ae242aa32288d892
AnnGerwel <60112631+AnnGerwel@users.noreply.github.com>
2020-05-15 00:54:43 +0300

    Add files via upload

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'core/model.php';      //подключение файла model.php
+require_once 'core/view.php';      //подключение файла view.php
+require_once 'core/controller.php';      //подключение файла controller.php
+require_once 'core/route.php';      //подключение файла с классом маршрутизатора
+Route::start();     // запуск маршрутизатора
